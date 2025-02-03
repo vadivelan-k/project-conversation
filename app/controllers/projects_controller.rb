f42b2d833d7dac79dc395c257a637a974b81eec2
Vadivelan Kuppusamy <reachme.vadivelan@gmail.com>
2025-02-03 21:44:05 +0800

    Add show page to display project and comments

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,17 @@
 class ProjectsController < ApplicationController
+  before_action :check_project_availability, only: [:show]
   def index
     @projects = Project.all
+  end
+
+  def show
+  end
+
+  private
+
+  def check_project_availability
+    @project = Project.find_by(id: params[:id])
+
+    return redirect_to projects_path if @project.nil?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def display_name
+    "#{first_name}, #{last_name}"
+  end
 end
